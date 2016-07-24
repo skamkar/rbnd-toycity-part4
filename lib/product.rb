68800a9aabddc2a5a5f0120510962e76702deefa
skamkar <sjk@zestfinance.com>
2016-07-23 23:05:18 -0700

    Add baseline functionality to udacudata class

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,10 @@
 require_relative 'udacidata'
 
 class Product < Udacidata
-  attr_reader :id, :price, :brand, :name
+  attr_reader :id
+  attr_accessor :price, :brand, :name
 
-  def initialize(opts={})
-
+  def initialize(opts={}) 
     # Get last ID from the database if ID exists
     get_last_id
     # Set the ID if it was passed in, otherwise use last existing ID
@@ -15,7 +15,6 @@ class Product < Udacidata
     @brand = opts[:brand]
     @name = opts[:name]
     @price = opts[:price]
-
   end
 
   private
@@ -24,7 +23,7 @@ class Product < Udacidata
     # If it exists, increment and use this value
     # Otherwise, use 0 as starting ID number
     def get_last_id
-      file = $DATA_PATH
+      file = @@data_path
       last_id = File.exist?(file) ? CSV.read(file).last[0].to_i + 1 : nil
       @@count_class_instances = last_id || 0
     end
