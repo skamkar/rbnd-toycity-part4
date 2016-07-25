acb665193d8081ff0ffa617427890e6b43eff59e
skamkar <sjk@zestfinance.com>
2016-07-25 12:33:32 -0700

    Fix random product creation with faker

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,16 +4,9 @@ require 'faker'
 # fake data for testing purposes
 
 def db_seed
-
-  ids = [1,2,3,4,5,6,7,8]
-  names = ["Aerodynamic Concrete Bag", "Gorgeous Bronze Bag", "Rustic Bronze Pants", "Synergistic Cotton Bag", "Heavy Duty Wooden Bag", "Orchid Plant", "Gorgeous Concrete Pants", "Incredible Paper Car", "Awesome Bronze Table"]
-  brands = ["Lakin Group", "Miller and Sons", "Langosh-Jerde", "Johnson and Sons", "Weber Group", "ColtToys","Bergstrom Inc","Kling and Sons","Mitchell, Rowe and Gottlieb"]
-  prices = [56.35, 29.72, 74.79, 63.73, 90.08, 2.0, 23.15, 57.49, 9.41]
-
-  ids.length.times do |i|
-    Product.create( id: ids[i],
-                    brand: brands[i],
-                    name: names[i],
-                    price: prices[i] )
+  10.times do
+    Product.create( brand: Faker::Company.name, 
+                    name: Faker::Commerce.product_name, 
+                    price: Faker::Commerce.price )
   end
 end
